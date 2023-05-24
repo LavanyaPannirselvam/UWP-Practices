979b0ca0df1a01d2692808566c7abde7204cf69b
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-05-24 05:59:39 +0530

    Add files via upload

---
+++ AsynchronousProgrammind/AsynchronousProgrammind/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace AsynchronousProgramming
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            Console.WriteLine("From main method.....");
+            await CallExampleAsync();
+            Console.WriteLine("Exit main method");
+        }
+
+        static async Task CallExampleAsync()
+        {
+            Console.WriteLine("From call method...");
+            await ExampleAsync();
+            Console.WriteLine("Exit call method");
+        }
+        static async Task ExampleAsync()
+        {
+            Console.WriteLine("From async method...");
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            Console.WriteLine("Exit async method");
+        }
+
+
+}
+}
+    
+
