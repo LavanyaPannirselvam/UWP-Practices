0eddcd0aa144669e6f6544518c2102ee686271ce
LavanyaPannirselvam <88311895+LavanyaPannirselvam@users.noreply.github.com>
2023-05-24 06:04:03 +0530

    Add files via upload

---
+++ AsynchronousProgramming/AsynchronousProgramming/CancellationExampleMethod1.cs
@@ -0,0 +1,27 @@
+﻿namespace AsynchronousProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Execute();
+            Console.WriteLine("bye");
+            Console.ReadKey();
+        }
+        static async void Execute()
+        {
+            int t = await Calculate(10, 20);
+            Console.WriteLine(t);
+            Console.WriteLine("done");
+
+        }
+        static async Task<int> Calculate(int x, int y)
+        {
+            Console.WriteLine("start");
+            await Task.Delay(4000);
+            Console.WriteLine("End");
+            return x + y;
+        }
+    }
+}
+
---
+++ AsynchronousProgramming/AsynchronousProgramming/Program.cs
@@ -0,0 +1,53 @@
+﻿//namespace AsynchronousProgramming
+//{
+//    class Program
+//    {
+//        public static async Task Main(string[] args)
+//        {
+//            Console.WriteLine("start");
+//            CancellationTokenSource cts = new(5000);
+//            CancellationToken cancellationToken = cts.Token;
+//            try
+//            {
+//                await Task.Run(async () =>
+//                {
+//                    int i = 1;
+//                    Console.WriteLine(i++);
+//                    await Task.Delay(1000);
+//                    if(cancellationToken.IsCancellationRequested)
+//                    {
+//                        throw new OperationCanceledException();
+//                    }
+
+//                },
+//                cancellationToken);
+//            }
+//            catch(OperationCanceledException)
+//            {
+//                Console.WriteLine("task cancelled");
+//            }
+//            //cts.Cancel();
+//        }
+
+
+//        //public static void Main(string[] args)
+//        //{
+//        //    Console.WriteLine("Main Method Started......");
+//        //    SomeMethod();
+//        //    Console.WriteLine("Main Method End");
+//        //    Console.ReadKey();
+//        //}
+//        //public async static void SomeMethod()
+//        //{
+//        //    Console.WriteLine("Some Method Started......");
+//        //    await Wait();
+//        //    Console.WriteLine("Some Method End");
+//        //}
+//        //private static async Task Wait()
+//        //{
+//        //    await Task.Delay(TimeSpan.FromSeconds(10));
+//        //    Console.WriteLine("\n10 Seconds wait Completed\n");
+//        //}
+//    }
+//}
+
